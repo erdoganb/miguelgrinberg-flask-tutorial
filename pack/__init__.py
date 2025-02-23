fae086b5097db0dff8aa52ce260fb49a0f9d87fb
erdoganb <erdoganborklu@gmail.com>
2025-02-23 22:52:21 +0300

    receiving form data -done

--- pack/__init__.py
+++ pack/__init__.py
@@ -2,5 +2,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "you-will-never-guess-oh-yes!"
 
 from pack import routes
--- pack/routes.py
+++ pack/routes.py
@@ -1,5 +1,6 @@
 from pack import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from pack.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -23,7 +24,22 @@ def time():
     import calendar
     now = datetime.datetime.now()
 
-    date_text = f"{now.day, calendar.month_name[now.month], now.year}"
-    time_text = now.hour
+    date_text = now.day, calendar.month_name[now.month], now.year
+    time_text = now.hour, now.minute
+    datetime = {'date' :date_text, 'time' :time_text}
 
-    return f"Date is: {date_text, time_text}" 
+    return render_template ("time.html", title="Date-Time", datetime=datetime)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        print('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect ('/index')
+    else:
+        print("ggwp")
+    return render_template('login.html', title="Sign in!", form=form)
