e7dabae79e3cad29f7d618df036de777af24fab8
erdoganb <erdoganborklu@gmail.com>
2025-02-26 21:08:24 +0300

    database - done

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from pack import app
+from pack import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from pack.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
